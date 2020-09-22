894230d97ce918220e88411203e76f7db346f856
YUNHE WANG <loganwang@YUNHEs-MacBook-Pro.local>
2020-09-22 22:27:38 +1000

    include the local and prod deploy

--- blogproject/settings/common.py
+++ blogproject/settings/common.py
@@ -13,19 +13,11 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '0d0p^=iw5)=simly4a7#v@dt6g96y8dr=abx1w!*hbuh_pu%b8'
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost ', '.zmrenwu.com','public IPv4','3.83.95.34']
 
 
 # Application definition
---
+++ blogproject/settings/local.py
@@ -0,0 +1,9 @@
+from .common import *
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = '0d0p^=iw5)=simly4a7#v@dt6g96y8dr=abx1w!*hbuh_pu%b8'
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+ALLOWED_HOSTS = ['*']
