32cc0d734b0a9dda5f5198e0a712a5b00ff436a9
YUNHE WANG <loganwang@YUNHEs-MacBook-Pro.local>
2020-09-27 20:56:24 +1000

    docker included

--- blogproject/settings/production.py
+++ blogproject/settings/production.py
@@ -1,7 +1,7 @@
 from .common import *
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'tinipj%_=w2b_!=w#%7x3*ixl-+sfmd=h9_q! (t93!#f)**pjq^t'
+SECRET_KEY = 'tinipj%_=w2b_!=w#%7x3*ixl-+sfmd=h9_q!(t93!#f)**pjq^t'
 #'0d0p^=iw5)=simly4a7#v@dt6g96y8dr=abx1w!*hbuh_pu%b8'
 
 # SECURITY WARNING: don't run with debug turned on in production!
