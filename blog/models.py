66ea824d2cc091e32d5c4aad3414d79ba9957dae
YUNHE WANG <loganwang@YUNHEs-MacBook-Pro.local>
2020-10-05 22:48:45 +1100

    post view function added

--- blog/models.py
+++ blog/models.py
@@ -94,5 +94,11 @@ class Post(models.Model):
     # Category 类似。
     author = models.ForeignKey(User, verbose_name='作者', on_delete=models.CASCADE)
 
+    views = models.PositiveIntegerField(default=0, editable=False)
+
+    def increase_views(self):
+        self.views += 1
+        self.save(update_fields=['views'])
+
     def get_absolute_url(self):
         return reverse('blog:detail', kwargs={'pk': self.pk})
